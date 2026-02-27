020591554c4db2716119336bf87dc84b72d3b5d7
anastaciomarbella <anastaciomarbella19@gmail.com>
2026-02-26 18:29:18 -0600

    cambios

--- src/pages/agendaDiaria.jsx
+++ src/pages/agendaDiaria.jsx
@@ -11,6 +11,7 @@ const AgendaDiaria = () => {
   const [loading, setLoading] = useState(true);
 
   const token = localStorage.getItem("token");
+  console.log("TOKEN:", token);
 
   const fetchCitas = async () => {
     try {
