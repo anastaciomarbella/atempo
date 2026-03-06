32025f9f915b1c4f1fd6c836765b594e8be82df4
anastaciomarbella <anastaciomarbella19@gmail.com>
2026-03-06 12:11:09 -0600

    publico

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./header.css";
-import { API_URL } from "../../config/api";
+import { API_URL } from "../../config";
 
 const Header = () => {
 
