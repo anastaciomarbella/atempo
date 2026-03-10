225fb9480443dead5a6d5864a46b4cce65eb160e
anastaciomarbella <anastaciomarbella19@gmail.com>
2026-03-09 19:10:51 -0600

    cambios en logincss

--- src/components/modalCita/modalCita.jsx
+++ src/components/modalCita/modalCita.jsx
@@ -3,8 +3,8 @@ import { FaTimes, FaSave, FaTrash } from "react-icons/fa";
 import "./modalCita.css";
 
 const coloresDisponibles = [
-  "#f16b74", "#56fa47", "#f58225", "#bbf7d0",
-  "#00fca8", "#47f183", "#58b4f1", "#3dc2ff",
+  "#f16b74", "#56fa47", "#f58225", "#843be4",
+  "#e700fc", "#ee0a0a", "#f3e40f", "#3dc2ff",
 ];
 
 function convertirA24h(hora) {
